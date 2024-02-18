e9a1284b30f05cba8cccd69df5eea19235d99a8b
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-02-17 23:34:40 -0800

    Create 252. Meeting Rooms Easy.cpp

---
+++ 252. Meeting Rooms Easy.cpp
@@ -0,0 +1,41 @@
+/*
+0 - 6 months
+Palo Alto Networks
+2
+6 months - 1 year
+Google
+2
+eBay
+2
+Apple
+2
+1 year - 2 years
+Amazon
+4
+Bloomberg
+2
+Oracle
+2
+Facebook
+*/
+
+Given an array of meeting time intervals where intervals[i] = [starti, endi],
+determine if a person could attend all meetings.
+
+ 
+
+Example 1:
+
+Input: intervals = [[0,30],[5,10],[15,20]]
+Output: false
+Example 2:
+
+Input: intervals = [[7,10],[2,4]]
+Output: true
+ 
+
+Constraints:
+
+0 <= intervals.length <= 104
+intervals[i].length == 2
+0 <= starti < endi <= 106
