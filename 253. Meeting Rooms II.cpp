7182d90319f9549ec14e3d574d4edd4cb651fef2
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-02-18 02:46:22 -0800

    Create 253. Meeting Rooms II.cpp

---
+++ 253. Meeting Rooms II.cpp
@@ -0,0 +1,75 @@
+/*
+  0 - 6 months
+Facebook
+13
+IBM
+10
+Bloomberg
+8
+Google
+7
+TikTok
+7
+Microsoft
+4
+Amazon
+4
+Oracle
+3
+Pinterest
+2
+Yandex
+2
+6 months - 1 year
+Apple
+3
+Snapchat
+2
+Salesforce
+2
+Goldman Sachs
+2
+Miro
+2
+1 year - 2 years
+Uber
+7
+eBay
+5
+Walmart Labs
+4
+PayPal
+3
+Visa
+3
+ByteDance
+3
+Cruise Automation
+2
+Splunk
+2
+ServiceNow
+2
+Workday
+2
+  */
+
+
+Given an array of meeting time intervals intervals where intervals[i] = [starti, endi], return the minimum number of conference rooms required.
+
+ 
+
+Example 1:
+
+Input: intervals = [[0,30],[5,10],[15,20]]
+Output: 2
+Example 2:
+
+Input: intervals = [[7,10],[2,4]]
+Output: 1
+ 
+
+Constraints:
+
+1 <= intervals.length <= 104
+0 <= starti < endi <= 106
